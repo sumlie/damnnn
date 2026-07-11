752a8892628819d0e4d4f67de229dccb93595854
summerlie <summerlie@proton.me>
2026-07-11 14:44:24 +0300

    chore: migrate from vite to rsbuild, and tailwindcss

---
+++ rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@rsbuild/core"; 
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginTailwindcss } from "@rsbuild/plugin-tailwindcss";
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginTailwindcss(),
+  ],
+  html: {
+    title: "damnnn",
+  },
+  output: {
+    distPath: {
+      root: "../dist",
+    },
+  },
+});
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+function App() {
+  return (
+    <div className="text-red-500">rsbuild + react + tw</div>
+  );
+}
+
+export default App;
